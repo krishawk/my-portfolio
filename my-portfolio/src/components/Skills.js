9c6f907d4fa04a637ec3e139cc01db0a6392c7e6
Kristie <Krishawkins920@gmail.com>
2023-04-30 20:15:57 -0500

    Add project files.

---
+++ my-portfolio/src/components/Skills.js
@@ -0,0 +1,37 @@
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React from "react";
+import { skills } from "../data";
+
+
+
+export default function Skills() {
+    return (
+        <section id="skills">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+                    <ChipIcon className="w-10 inline-block mb-4" />
+                    <h1 clssName="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                        Skills &amp; Technologies
+                    </h1>
+                    <p className="text-base leading-relaxed xl:w-2/4 mx-auto">
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                        Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in
+                        their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    </p>
+                </div>
+                <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                    {skills.map((skill) => (
+                        <div key={skill} className="p-2 sm:w-1/2 w-full">
+                            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                                <BadgeCheckIcon className="title-font font-medium text-white" />
+                                <span className="title-font font-medium text-white">
+                                    {skill}
+                                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
